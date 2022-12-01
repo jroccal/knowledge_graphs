3ef405ccf86878bff8204bc74cf0369236b01395
Jaime <jroccalayza@gmail.com>
2022-12-01 16:47:21 -0500

    change parameter

--- src/plot_graphs.py
+++ src/plot_graphs.py
@@ -11,20 +11,20 @@ def softmax(x):
 def get_softmax_norm(x, slope=1.0, scale=200):                     
     return scale*softmax(np.log(x) + 1.0)
 
-def create_graph(df, edges_norm=False, lower_bound_edges=None):
+def create_graph(df, edges_norm=False, min_degree_node=None):
     net = Network()
     
     df = df.groupby(['src', 'dst']).sum()
     display(df.head())
     list_index = list(dict(df.index).keys())    
-    if lower_bound_edges is not None:
+    if min_degree_node is not None:
         series_count_src = df.reset_index()['src'].value_counts()
-        list_index = series_count_src[series_count_src > lower_bound_edges].index
+        list_index = series_count_src[series_count_src >= min_degree_node].index
             
     if edges_norm:
         df['edge_norm'] =  get_softmax_norm(df['edge'])
         
-    print('lenght src nodes', len(list_index), lower_bound_edges)
+    print('lenght src nodes:', len(list_index), 'min_degree_node:', min_degree_node)
     print(df.shape)
         
     for index in list_index:            
